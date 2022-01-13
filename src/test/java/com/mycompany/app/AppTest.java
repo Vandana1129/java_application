c342983ee91c7bb6528bfdec64407084c1ceff92
Vandana1129 <95488982+Vandana1129@users.noreply.github.com>
2022-01-13 08:04:10 -0700

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -16,6 +16,8 @@ public class AppTest
 
     @Test
     public void twoAndThreeIsFive() throws Exception {
+        int abc;
+        string yx;
         final long result = new App().add(2, 3);
         assertThat(result, is(5L));
     }
